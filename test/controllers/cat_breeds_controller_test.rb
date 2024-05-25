03b614a39b8c6c28fed43f60224251755f95d158
Killian Portier <killian.portier@yahoo.fr>
2024-05-25 20:58:53 +0200

    test: started to create test for cat breeds controller, add gem for do test on controller and create some view

--- test/controllers/cat_breeds_controller_test.rb
+++ test/controllers/cat_breeds_controller_test.rb
@@ -1,4 +1,19 @@
 require "test_helper"
 
 class CatBreedsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @cat_breed = cat_breeds(:one)
+  end
+
+  test "should get index" do
+    get cat_breeds_url
+    assert_response :success
+    assert_not_nil assigns(:cat_breeds)
+  end
+
+  test "should show cat_breed" do
+    get cat_breed_url(@cat_breed)
+    assert_response :success
+    assert_not_nil assigns(:cat_breed)
+  end
 end
