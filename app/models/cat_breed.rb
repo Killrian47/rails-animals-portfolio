51ef1e07a3b6de7aa14c403753b1107676a737b3
Killian Portier <killian.portier@yahoo.fr>
2024-05-26 15:03:23 +0200

    feat: create the seed for getting cat breeds and correct the format of cate breed validation model

--- app/models/cat_breed.rb
+++ app/models/cat_breed.rb
@@ -1,4 +1,4 @@
 # The CatBreed model is used to store the cat breed name.
 class CatBreed < ApplicationRecord
-  validates :name, presence: true, uniqueness: true, length: { in: 3..50 }, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :name, presence: true, uniqueness: true, length: { in: 3..50 }, format: { with: /\A[^0-9][\w\s-]+\z/ }
 end
