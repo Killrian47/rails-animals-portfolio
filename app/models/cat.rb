494e2c8493ed7753421dbbffedec2fd5b415b3e7
Killian Portier <killian.portier@yahoo.fr>
2024-05-26 15:12:56 +0200

    feat: cat model was created

---
+++ app/models/cat.rb
@@ -0,0 +1,4 @@
+# Description: Cat model
+class Cat < ApplicationRecord
+  belongs_to :cat_breed
+end
--- app/models/cat_breed.rb
+++ app/models/cat_breed.rb
@@ -1,4 +1,5 @@
 # The CatBreed model is used to store the cat breed name.
 class CatBreed < ApplicationRecord
+  has_many :cats, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: { in: 3..50 }, format: { with: /\A[^0-9][\w\s-]+\z/ }
 end
