ceb495786194f871d4d386d69dc43e650338cbfc
Killian Portier <killian.portier@yahoo.fr>
2024-05-26 16:03:45 +0200

    test: create test for cat model and implement it for pass the tests

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,7 @@
 # Description: Cat model
 class Cat < ApplicationRecord
   belongs_to :cat_breed
+  validates :name, presence: true, length: { in: 3..50 }, format: { with: /\A[a-zA-Z\s]+\z/ }
+  validates :age, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :cat_breed, presence: true
 end
